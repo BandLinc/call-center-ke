2bc739c409d6efb286a568ae0d14187c0c160eb6
LincolnLidanya <lincolnlidanya@gmail.com>
2020-11-15 08:17:18 +0300

    Resoved errors in app component, deleted hppt import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';

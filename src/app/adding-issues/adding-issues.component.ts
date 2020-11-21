48029456ee6dd7aec931f86996eaec260cb40644
mbuguap928 <mbuguap928@gmail.com>
2020-11-21 19:59:35 +0300

    added adding-issues component

---
+++ src/app/adding-issues/adding-issues.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adding-issues',
+  templateUrl: './adding-issues.component.html',
+  styleUrls: ['./adding-issues.component.css']
+})
+export class AddingIssuesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

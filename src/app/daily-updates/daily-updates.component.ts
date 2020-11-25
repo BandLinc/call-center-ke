b38d5712dd2b177ae3b156f42272da781affa889
LincolnLidanya <lincolnlidanya@gmail.com>
2020-11-25 18:02:06 +0300

    Added daily issues in the daily updates component

--- src/app/daily-updates/daily-updates.component.ts
+++ src/app/daily-updates/daily-updates.component.ts
@@ -1,15 +1,20 @@
+import { PendingissuesService } from './../services/pendingissues.service';
 import { Component, OnInit } from '@angular/core';
+import { Pendingissues } from '../classes/pendingissues';
 
 @Component({
   selector: 'app-daily-updates',
   templateUrl: './daily-updates.component.html',
-  styleUrls: ['./daily-updates.component.css']
+  styleUrls: ['./daily-updates.component.css'],
 })
 export class DailyUpdatesComponent implements OnInit {
+  lstIssues: Pendingissues[];
 
-  constructor() { }
+  constructor(private _pendingIssues: PendingissuesService) {}
 
   ngOnInit(): void {
+    this._pendingIssues.getPendingIssues().subscribe((data) => {
+      this.lstIssues = data;
+    });
   }
-
 }
